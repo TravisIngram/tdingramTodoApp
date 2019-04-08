1ba9fc42dce2733b4e574f711c37350fe8d157c3
TravisIngram <ingramtravis@gmail.com>
2019-04-08 14:59:10 -0500

    Add helper function for search term submit

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,8 +1,10 @@
 import { Template } from "meteor/templating";
 import { Tasks } from "../api/tasks.js";
+import { Session } from "meteor/session";
 
 import "./task.js";
 import "./body.html";
+import "./searchResult.js";
 
 // Here we subscribe to the data returned by tasks.all defined in the publish method
 Meteor.subscribe("tasks.all");
@@ -21,11 +23,23 @@ Template.body.events({
 
     // Clear form
     target.text.value = "";
+  },
+
+  "submit .search-tasks"(event) {
+    event.preventDefault();
+
+    const searchTarget = event.target;
+    const searchString = searchTarget.text.value;
+
+    // Set a session value for the search term to make it available to other methods
+    Session.set("searchString", searchString);
+
+    searchTarget.text.value = "";
   }
 });
 
 Template.body.helpers({
-  tasks() {
+  tasks: function() {
     // Show newest tasks at the top
     return Tasks.find({}, { sort: { createdAt: -1 } });
   }
