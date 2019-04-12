eb11d8d6810078f15e674f74c4b62f7fca8434ee
TravisIngram <ingramtravis@gmail.com>
2019-04-12 18:45:18 -0500

    Add EasySearch index for the tasks collection

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,3 +1,10 @@
-import { Mongo } from 'meteor/mongo';
+import { Mongo } from "meteor/mongo";
+import { Index, MongoDBEngine } from "meteor/easy:search";
 
-export const Tasks = new Mongo.Collection('tasks');
+export const Tasks = new Mongo.Collection("tasks");
+
+export const TasksIndex = new Index({
+  collection: Tasks,
+  fields: ["text"],
+  engine: new MongoDBEngine()
+});
