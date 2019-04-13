726c91feff9317f90e6a4cd16f8c3e8956fc86b1
TravisIngram <ingramtravis@gmail.com>
2019-04-13 01:53:12 -0500

    Add comments to helpers and index creation

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -3,6 +3,7 @@ import { Index, MongoDBEngine } from "meteor/easy:search";
 
 export const Tasks = new Mongo.Collection("tasks");
 
+// Create a new instance of ES targeting the tasks collection
 export const TasksIndex = new Index({
   collection: Tasks,
   fields: ["text"],
--- imports/ui/searchResult.js
+++ imports/ui/searchResult.js
@@ -4,11 +4,13 @@ import { TasksIndex } from "../api/tasks.js";
 
 import "./searchResult.html";
 
+// Return the instance of the EasySearch index we created
 Template.searchResult.helpers({
   tasksIndex: function() {
     return TasksIndex;
   },
 
+  // Return the value of 'showResults' from the session store.
   showResults: function() {
     return Session.get("showResults");
   }
